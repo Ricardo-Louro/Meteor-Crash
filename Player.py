f64fcba00d833fc880b9323a48613ac784405c32
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-24 12:38:05 +0100

    Reworked movement to take friction into account if it's on the ground. If it's not, it takes gravity into account.

--- Player.py
+++ Player.py
@@ -8,34 +8,33 @@ class Player(Object):
     radius = 20
     mass = 5
 
-    normal_force = self.mass * gravity 
+    force_applied = 0
 
+    time = 1/60
 
-
-        self.position = (self.position[0] + self.velocity_x, self.position[1] + self.velocity_x)
-        if grounded:
-            self.velocity_x *= 0.5
-
+    def __init__(self):
+        super().__init__()
+        self.normal_force = self.mass * self.gravity
+        
             
-    def Move(self, direction):
-        coefficient_of_static_friction=0.2
-        static_friction = coefficient_of_static_friction * normal_force
-        net_force = force_applied - static_friction
-        if(net_force>0):
-           #kinetic friction
-           coefficient_of_kinetic_friction = 0.05
-           kinetic_friction = coefficient_of_kinetic_friction * normal_force
-           net_force = force_applied - kinetic_friction
-           acceleration = net_force / ball_mass
-           self.velocity_x = acceleration * t
+    def Move(self):
+        print(self.grounded)
+        if(self.grounded):
+            self.velocity_y = 0
+            coefficient_of_static_friction=0.2
+            static_friction = coefficient_of_static_friction * self.normal_force
+            net_force = self.force_applied - static_friction
+            
+            #kinetic friction
+            coefficient_of_kinetic_friction = 0.05
+            kinetic_friction = coefficient_of_kinetic_friction * self.normal_force
+            net_force = self.force_applied - kinetic_friction
+            acceleration = net_force / self.mass
+            self.velocity_x = acceleration * self.time
         else:
-            self.velocity_x=0   
+            self.velocity_y = self.gravity
+            
+        self.position = (self.position[0] + self.velocity_x, self.position[1] + self.velocity_y)
 
     def Draw(self, surface):
-        pygame.draw.circle(surface, self.colour, self.position,
-
-    def GroundCheck(self, groundHeight):
-        self.position[0] >= groundHeight - self.radius
-            self.grounded = true
-        else:
-            self.grounded = false
+        pygame.draw.circle(surface, self.colour, self.position, self.radius)
