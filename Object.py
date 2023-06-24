b11957e793bc70346b47a84d4f36a863ec517e0b
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-24 12:36:32 +0100

    Incorporated more variables and methods that will be shered between the objects in the game

--- Object.py
+++ Object.py
@@ -6,4 +6,12 @@ class Object:
     velocity_x = 0
     velocity_y = 0
     gravity = 9.8
+    grounded = False
+
+    def GroundCheck(self, groundHeight):
+        if self.position[1] >= (groundHeight - self.radius):
+            self.grounded = True
+            self.position = (self.position[0], groundHeight - self.radius)
+        else:
+            self.grounded = False
     
